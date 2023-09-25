3d256b186a01fe48dae11a95ceb8c540b1da8aa2
Magno <magnop64@gmail.com>
2023-09-25 20:44:17 -0300

    inicio

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join (__dirname, 'style')]
+  }
 }
 
 module.exports = nextConfig
